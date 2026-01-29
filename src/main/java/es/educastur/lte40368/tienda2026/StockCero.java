d57e1c3dd616e182de30b89f78a6fcaa2e61c860
1dawd18 <1dawd18@A204PC18.desarrolloweb.local>
2026-01-29 13:36:36 +0100

    Tienda2026/01/29

---
+++ src/main/java/es/educastur/lte40368/tienda2026/StockCero.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.educastur.lte40368.tienda2026;
+
+/**
+ *
+ * @author 1dawd18
+ */
+public class StockCero extends Exception{
+    public StockCero(String mensaje){
+        super(mensaje);
+    }
+}
