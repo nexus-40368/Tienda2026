e49c8c0577074f93892f9798fbab51c5ff7a9158
1dawd18 <1dawd18@A204PC18.desarrolloweb.local>
2026-01-29 13:36:48 +0100

    Tienda2026/01/29

---
+++ src/main/java/es/educastur/lte40368/tienda2026/StockInsuficiente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.educastur.lte40368.tienda2026;
+
+/**
+ *
+ * @author 1dawd18
+ */
+public class StockInsuficiente extends Exception{
+    public  StockInsuficiente (String mensaje){
+        super(mensaje);
+    }
+}
