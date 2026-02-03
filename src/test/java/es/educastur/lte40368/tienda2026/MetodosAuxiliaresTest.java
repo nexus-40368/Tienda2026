9e010385d89030cc567350cee56f011aadcc1e9c
1dawd18 <1dawd18@A204PC18.desarrolloweb.local>
2026-02-03 10:12:42 +0100

    2026/02/03 Caja negra:En la clase de hoy realizamos pruebas a los metodos

---
+++ src/test/java/es/educastur/lte40368/tienda2026/MetodosAuxiliaresTest.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package es.educastur.lte40368.tienda2026;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MetodosAuxiliaresTest {
+
+    public MetodosAuxiliaresTest() {
+    }
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    @BeforeEach
+    public void setUp() {
+    }
+    @AfterEach
+    public void tearDown() {
+    }
+    /**
+     * Test para esInt
+     */
+    @Test
+    public void testEsInt() {
+        assertTrue(MetodosAuxiliares.esInt("5"));
+        assertFalse(MetodosAuxiliares.esInt("55555555555555555555555555"));
+        assertTrue(MetodosAuxiliares.esInt("-5"));
+        assertFalse(MetodosAuxiliares.esInt("5.5"));
+        assertFalse(MetodosAuxiliares.esInt("sadasda"));;
+    }
+    /**
+     * Test para esDouble
+     */
+    @Test
+    public void testEsDouble() {
+        assertTrue(MetodosAuxiliares.esDouble("5"));
+        assertTrue(MetodosAuxiliares.esDouble("-5"));
+        assertTrue(MetodosAuxiliares.esDouble("5.5"));
+        assertFalse(MetodosAuxiliares.esDouble("sadasda"));;
+    }
+    /**
+     * Test para validarDni
+     */
+    @Test
+    public void testValidarDni() {
+        assertTrue(MetodosAuxiliares.validarDni("80580845T"));
+        assertTrue(MetodosAuxiliares.validarDni("36347775R"));
+        assertFalse(MetodosAuxiliares.validarDni("36347375B"));
+        assertFalse(MetodosAuxiliares.validarDni("36349975Y"));;
+    }
+    /**
+     * Test of calcularLetraDni method, of class MetodosAuxiliares.
+     */
+    @Test
+    public void testCalcularLetraDni() {
+    }
+
+}
